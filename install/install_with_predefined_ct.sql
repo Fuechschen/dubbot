d53db2971feef5e5c3cc9b931164dc4ef52b7ffd
Fuechschen <joh.diehl@gmail.com>
2015-10-22 01:40:05 +0200

    fixed some nullpointers, preparements for afk-removal

--- install/install_with_predefined_ct.sql
+++ install/install_with_predefined_ct.sql
@@ -12,18 +12,20 @@ CREATE TABLE IF NOT EXISTS `tracks` (
   KEY `id` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-CREATE TABLE IF NOT EXISTS `users` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `username` varchar(50) NOT NULL DEFAULT 'null',
-  `userid` varchar(50) NOT NULL DEFAULT 'null',
-  `roleid` varchar(50) NOT NULL DEFAULT '0',
-  `dubs` int(11) NOT NULL DEFAULT '0',
-  `status` int(11) NOT NULL DEFAULT '0',
-  `mcuuid` varchar(50) DEFAULT NULL,
-  `created_at` datetime DEFAULT NULL,
-  `updated_at` datetime DEFAULT NULL,
-  KEY `id` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+CREATE TABLE `users` (
+	`id` INT(11) NOT NULL AUTO_INCREMENT,
+	`username` VARCHAR(50) NOT NULL DEFAULT 'null',
+	`userid` VARCHAR(50) NOT NULL DEFAULT 'null',
+	`roleid` VARCHAR(50) NOT NULL DEFAULT '0',
+	`dubs` INT(11) NOT NULL DEFAULT '0',
+	`status` INT(11) NOT NULL DEFAULT '0',
+	`last_active` DATETIME NULL DEFAULT CURRENT_TIMESTAMP,
+	`afk` INT(11) NULL DEFAULT '0',
+	`warned_for_afk` INT(11) NULL DEFAULT '0',
+	`created_at` DATETIME NULL DEFAULT NULL,
+	`updated_at` DATETIME NULL DEFAULT NULL,
+	INDEX `id` (`id`)
+)ENGINE=InnoDB;
 
 CREATE TABLE IF NOT EXISTS `custom_texts` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
