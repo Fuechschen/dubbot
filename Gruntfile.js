fd31a71eb2b0377c958815e8da04eefc227a9f45
Fuechschen <me@fuechschen.org>
2016-01-27 21:38:08 +0100

    grunt...

---
+++ Gruntfile.js
@@ -0,0 +1,24 @@
+module.exports = function(grunt) {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        sonarRunner: {
+            analysis: {
+                options: {
+                    debug: true,
+                    separator: '\n',
+                    sonar: {
+                        host: {
+                            url: 'http://localhost:9036'
+                        },
+                        projectKey: 'sonar:dubbot',
+                        projectName: 'dubbot',
+                        projectVersion: '0.2.2',
+                        sources: ['test'].join(','),
+                        language: 'js',
+                        sourceEncoding: 'UTF-8'
+                    }
+                }
+            }
+        }
+    });
+};
